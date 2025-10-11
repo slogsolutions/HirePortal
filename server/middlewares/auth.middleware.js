2e2c856892d49058788b7614cfea1b4df1abfa1c
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-10-11 13:42:03 +0530

    changed the error message to check not authorized message

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -7,11 +7,11 @@ const protect = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   }
-  if (!token) return res.status(401).json({ message: 'Not authorized' });
+  if (!token) return res.status(401).json({ message: 'No Token available' });
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const user = await User.findById(decoded.id);
-  if (!user) return res.status(401).json({ message: 'Not authorized' });
+  if (!user) return res.status(401).json({ message: 'No user available' });
 
   req.user = user;
   next();
