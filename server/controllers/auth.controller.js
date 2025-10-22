cfbf7d09666d3ea71d11fb8a68eb8bc5664ce835
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-10-22 22:34:45 +0530

    removed console.log from auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -36,12 +36,12 @@ const verifyOtp = asyncHandler(async (req, res) => {
 const login = asyncHandler(async (req, res) => {
 
   const { email, password } = req.body;
-  console.log(email,password,"login");
+  // console.log(email,password,"login");
   const user = await User.findOne({ email });
-  console.log(user,"after find login");
+  // console.log(user,"after find login");
   if (!user) return res.status(401).json({ message: 'Invalid credentials' });
   const match = await bcrypt.compare(password, user.password);
-  console.log("match->",match,"password ->",password, "--- user.password->",user.password)
+  // console.log("match->",match,"password ->",password, "--- user.password->",user.password)
   if (!match) return res.status(401).json({ message: 'Invalid credentials2' });
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || '7d' });
   res.json({ token, user: { id: user._id, name: user.name, role: user.role, email: user.email } });
