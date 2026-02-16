1f12359bf84372e73a04f131ef0b1841392f6b94
Laksh Raj Kumar <lakshrajkumar@gmail.com>
2026-02-16 13:23:03 +0530

    Add Android assetlinks

--- server/app.js
+++ server/app.js
@@ -39,6 +39,25 @@ app.use("/api/salaries", salaryRoutes);
 app.use("/api/performance", performanceRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
+// Android App Links verification
+app.get("/.well-known/assetlinks.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send([
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: "com.slogsolutions.slogems",
+        sha256_cert_fingerprints: [
+          "65:96:7E:41:62:B8:EA:20:9E:5D:67:73:0B:D3:B7:AE:2F:A6:51:8B:C2:84:09:A4:A3:5C:15:61:75:EC:7B:59",
+          "16:95:FF:24:B6:3C:AE:BB:4D:E1:4C:46:7B:81:5F:D1:7F:C5:3F:80:41:6C:0B:7F:75:36:E7:09:D3:46:26:CE"
+        ]
+      }
+    }
+  ]);
+});
+
+
 app.get("/health", (req, res) => res.status(200).send("OK"));
 // app.get("/laksh", (req, res) => res.status(200).send("OK"));
 
